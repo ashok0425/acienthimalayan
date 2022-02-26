025389bad573ff8d23b0fb3b7e77baab7de96065
ashok0425 <ashokmehta123y@gmail.com>
2022-02-26 14:15:22 +0545

    footer completed

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,8 +9,8 @@
       <div class="row">
         <div class="col-lg-3 col-6">
           <!-- small box -->
-          
-          <div class="small-box bg-info">
+          <div class="alert alert-success">Login Successful</div>
+          {{-- <div class="small-box bg-info">
             <div class="inner">
               <h3>55</h3>
 
@@ -21,7 +21,7 @@
             </div>
             <a href="" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
-        </div>
+        </div> --}}
        
   </div>
   <!-- /.row -->
