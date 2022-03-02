b71c4e634658632e262c9cadcefb8a29830f21e4
ashok0425 <ashokmehta123y@gmail.com>
2022-03-02 13:33:20 +0545

    video option and location saving option added

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -227,21 +227,6 @@ $('.afflicated').owlCarousel({
 </script>
 @stack('scripts')
 
-<script>
-    // var x = document.getElementById("demo");
-    function getLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-        // x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-    }
-    
-    function showPosition(position) {
-       "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude
-    }
-    </script>
 </body>
 
 </html>
