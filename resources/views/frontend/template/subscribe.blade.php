be963c0bc8f745b10a4c1626ff2448830ae50d86
ashok0425 <ashokmehta123y@gmail.com>
2022-03-21 19:55:21 +0545

    updated

--- resources/views/frontend/template/subscribe.blade.php
+++ resources/views/frontend/template/subscribe.blade.php
@@ -1,10 +1,6 @@
 <style>
     
 .subscribe {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
 	background: white;
 	border-radius: 10px;
 	box-shadow: 0 4px 20px rgba(61, 159, 255, 0.2)
@@ -19,7 +15,6 @@
 }
 
 .subscribe__copy {
-	max-width: 450px;
 	text-align: center;
 	margin-bottom: 33px;
 	line-height: 1.5;
@@ -33,7 +28,6 @@
 	padding: 10px 25px;
 	border-radius: 5px;
  	border: 1px solid #CAD3DB;
-	width: 431px;
 	font-size: 18px;
 	color: rgb(42, 135, 183);
 }
@@ -46,7 +40,6 @@
 	background: #3D9FFF;
 	padding: 10px;
 	border: none;
-	width: 244px;
 	border-radius: 5px;
 	font-size: 18px;
 	color: white;
@@ -57,26 +50,33 @@
 	box-shadow: 0 10px 20px rgb(42, 135, 183);
 }
 
-.notice {
-	width: 345px;
-    margin: auto
-}
+
 
 </style>
-<div class="container-fluid mt-4">
-<div class="subscribe py-4">
-	<h2 class="subscribe__title custom-text-primary">Let's keep in touch</h2>
-	<p class="subscribe__copy">Subscribe to keep up with fresh news and exciting updates. We promise not to spam you!</p>
+<div class="subscribe container-fluid mt-4 py-4">
+	<div class="row">
+	<h2 class="subscribe__title custom-text-primary col-md-4 offset-md-4 text-center">Let's keep in touch</h2>
+	<p class="subscribe__copy col-md-4 offset-md-4 text-center">Subscribe to keep up with fresh news and exciting updates. We promise not to spam you!</p>
+</div>
     <form action="{{ route('subscribe.store') }}" method="POST">
         @csrf
 	<div class="form">
-		<input type="email" class="form__email" placeholder="Enter your email address" required name="email"/>
-		<button class="form__button" type="submit">Send</button>
+		<div class="row">
+			<div class="col-md-5 offset-md-3">
+				
+		<input type="email" class="form__email w-100" placeholder="Enter your email address" required name="email"/>
 	</div>
-	<div class="notice text-center">
+	<div class="col-md-2">
+		<button class="form__button w-100" type="submit">Send</button>
+	</div>
+
+	</div>
+</div>
+<div class="row">
+	<div class="notice col-md-4 text-center offset-md-4 ">
 		<input type="checkbox">
 		<span class="notice__copy ">I agree to my email address being stored and uses to recieve  newsletter.</span>
 	</div>
-</form>
 </div>
+</form>
 </div>
