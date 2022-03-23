a2e5d56bfd0e7f67b2d7d63d8ba2176c3818471a
ashok0425 <ashokmehta123y@gmail.com>
2022-03-23 08:40:18 +0545

    testimonial see more updated

--- resources/views/frontend/package_detail.blade.php
+++ resources/views/frontend/package_detail.blade.php
@@ -634,12 +634,12 @@ color: #fff!important;
 <!-- Modal -->
 <div class="modal fade " id="video" tabindex="-1" aria-labelledby="videoy" aria-hidden="true">
     <div class="modal-dialog modal-md">
-      <div class="modal-content">
-        <div class="modal-header">
+      <div class="modal-content bg-transparent">
+        <div class="modal-header bg-transparent">
           
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body ">
+        <div class="modal-body bg-transparent">
        
 <div style="width:200px!important;">
     {!! $package->video!!}
